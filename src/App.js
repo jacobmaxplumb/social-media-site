cbe22307a9f7c512d145c9db5cf9f7abc12a5705
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-08-26 15:15:22 -0400

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { AppBar, makeStyles, Toolbar } from '@material-ui/core';
+import Home from './components/Home';
+import NewsFeed from './components/NewsFeed';
+import Profile from './components/Profile';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    textDecoration: 'none',
+    color: 'white'
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Link className={classes.menuButton} to="/">Home</Link>
+            <Link className={classes.menuButton} to="/profile">Profile</Link>
+            <Link className={classes.menuButton} to="/feed">News Feed</Link>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/feed">
+            <NewsFeed />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
